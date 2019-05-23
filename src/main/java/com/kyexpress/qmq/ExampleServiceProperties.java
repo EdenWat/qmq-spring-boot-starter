5ecdf7d8cfdcb80ada30c8a9098e951702692839
wangjian13 <wangjian13@ky-tech.com.cn>
2019-05-23 21:16:40 +0800

    Signed-off-by: wangjian13 <wangjian13@ky-tech.com.cn>

---
+++ src/main/java/com/kyexpress/qmq/ExampleServiceProperties.java
@@ -0,0 +1,28 @@
+package com.kyexpress.qmq;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * @author kye
+ */
+@ConfigurationProperties("example.service")
+public class ExampleServiceProperties {
+    private String prefix;
+    private String suffix;
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    public void setPrefix(String prefix) {
+        this.prefix = prefix;
+    }
+
+    public String getSuffix() {
+        return suffix;
+    }
+
+    public void setSuffix(String suffix) {
+        this.suffix = suffix;
+    }
+}
